b416fea01b4da0b6dc7263342a96595ef9271bb8
DESKTOP-2KRTAJU\GIGABYTE <1376221162@qq.com>
2021-10-22 16:14:52 +0800

    矿机列表过滤参数修改

--- src/modalFieldFiliter/miningMachineField.js
+++ src/modalFieldFiliter/miningMachineField.js
@@ -6,6 +6,7 @@ export const getMiningMachineListField = (filiterField = []) => {
         'name',
         'state',
         'model',
+        'start',
         'config',
         'price',
         'core',
